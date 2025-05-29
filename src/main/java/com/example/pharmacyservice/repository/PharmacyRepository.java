1dbc2a72bbeecee50d6366c3f8605202ab9573d5
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-29 12:36:31 +0530

    created existsByAdminId method in repository

--- src/main/java/com/example/pharmacyservice/repository/PharmacyRepository.java
+++ src/main/java/com/example/pharmacyservice/repository/PharmacyRepository.java
@@ -1,7 +1,9 @@
 package com.example.pharmacyservice.repository;
 
 import com.example.pharmacyservice.module.Pharmacy;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PharmacyRepository extends JpaRepository<Pharmacy,String> {
+    boolean existsByAdminId(@NotBlank Long adminId);
 }
